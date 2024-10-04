cd348ce2ad2f4331df14d5b72f026cfbbaf258cd
Bảo Bảo <baobao@Baos-MacBook-Pro.local>
2024-10-05 02:18:21 +0700

    hello world voi express

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
-const http = require("http");
+const express = require("express");
+const app = express();
+const port = 3000;
 
-const hostname = "localhost"; //localhost127.0.0.1
-const port = 3000; // backend: port 8080
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n");
-});
-//server.listen(port, hostname, callback) is the same as server.listen(port, hostname).on('listening', callback)
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-// kill the server: 
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
